29d3a12cfee5116094baef2320438eceb9ba7c08
Dan Jenkins <dan.jenkins88@gmail.com>
2012-09-18 21:05:46 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var AsteriskAmi = require('asterisk-ami');
+
+var ami = new AsteriskAmi( { host: '172.16.172.131', username: 'astricon', password: 'secret' } );
+
+ami.on('ami_data', function(data){
+  console.log('AMI DATA', data);
+});
+
+ami.connect(function(response){
+  ami.send({action: 'Ping'});//run a callback event when we have connected to the socket
+});
+
+process.on('SIGINT', function () {
+  ami.disconnect();
+});
