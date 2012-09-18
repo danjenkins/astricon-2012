1527ef513704d54bc6cb1b13da9a252a541b985c
Dan Jenkins <dan.jenkins88@gmail.com>
2012-09-18 21:45:26 +0100

    added more functionality into server, now serves up html and outputs to html via nowjs

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 var AsteriskAmi = require('asterisk-ami');
+var nowjs = require("now");
+var fs = require('fs');
 
 var ami = new AsteriskAmi( { host: '172.16.172.131', username: 'astricon', password: 'secret' } );
 
+var httpServer = require('http').createServer(function(req, res){ 
+  var stream = fs.createReadStream(__dirname + '/webpage.html');
+  stream.on('error', function (err) {
+      res.statusCode = 500;
+      res.end(String(err));
+  });
+  stream.pipe(res);
+});
+
+httpServer.listen(8080);
+
+var everyone = nowjs.initialize(httpServer);
+
 ami.on('ami_data', function(data){
-  console.log('AMI DATA', data);
+  if(everyone.now.echoAsteriskData instanceof Function){
+    everyone.now.echoAsteriskData(data);
+  }
 });
 
 ami.connect(function(response){
-  ami.send({action: 'Ping'});//run a callback event when we have connected to the socket
+  setInterval(function(){
+    ami.send({action: 'Ping'});//run a callback event when we have connected to the socket
+  }, 1000);
 });
 
 process.on('SIGINT', function () {
   ami.disconnect();
+  process.exit(0);
 });
