3375237e5508d72d64924ae8ef3c799a6e74431f
Dan Jenkins <dan.jenkins88@gmail.com>
2012-09-19 09:41:39 +0100

    added express

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 var AsteriskAmi = require('asterisk-ami');
 var nowjs = require("now");
 var fs = require('fs');
+var express = require('express');
+var http = require('http');
+
 
 var ami = new AsteriskAmi( { host: '172.16.172.131', username: 'astricon', password: 'secret' } );
+var app = express();
+var server = http.createServer(app).listen(8080);
 
-var httpServer = require('http').createServer(function(req, res){ 
+app.configure(function(){
+  app.use("/assets", express.static(__dirname + '/assets'));
+});
+
+app.use(express.errorHandler());
+app.get('/', function(req, res) {
   var stream = fs.createReadStream(__dirname + '/webpage.html');
   stream.on('error', function (err) {
       res.statusCode = 500;
       res.end(String(err));
   });
   stream.pipe(res);
-});
+})
 
-httpServer.listen(8080);
-
-var everyone = nowjs.initialize(httpServer);
+var everyone = nowjs.initialize(server);
 
 ami.on('ami_data', function(data){
   if(everyone.now.echoAsteriskData instanceof Function){
@@ -26,7 +34,7 @@ ami.on('ami_data', function(data){
 ami.connect(function(response){
   setInterval(function(){
     ami.send({action: 'Ping'});//run a callback event when we have connected to the socket
-  }, 1000);
+  }, 2000);
 });
 
 process.on('SIGINT', function () {
